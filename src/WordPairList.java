7eaee2a6a7c3c160f67a2916e910319cf4622080
oliviaw <oliviaw2026@cis.edu.hk>
2024-09-10 08:55:43 +0800

    yay!

---
+++ src/WordPairList.java
@@ -0,0 +1,30 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class WordPairList {
+    private ArrayList<WordPair> allPairs;
+
+    public WordPairList(String[] words) {
+        allPairs = new ArrayList<>();
+        for (int i=0; i<words.length; i++) {
+            for (int j=i+1; j<words.length; j++) {
+                allPairs.add(new WordPair(words[i],words[j]));
+            }
+        }
+    }
+
+    public int numMatches() {
+        int count = 0;
+        for (int i=0; i<allPairs.size(); i++) {
+            if (allPairs.get(i).getFirst().equals(allPairs.get(i).getSecond())) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public ArrayList<WordPair> getAllPairs() {
+        return allPairs;
+    }
+
+}
